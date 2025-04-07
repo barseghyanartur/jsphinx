e5906fe3656cd92b2ba34b37ce759682283320da
Artur Barseghyan <artur.barseghyan@gmail.com>
2025-04-08 01:50:43 +0200

    Use better icons

--- src/js/download_adapter.js
+++ src/js/download_adapter.js
@@ -419,11 +419,12 @@ function initializeJsphinxFeatures() {
 
     function handleIcons() {
         // Define icons for the two states.
-        const collapsedIcon = '👁';    // When code is collapsed.
-        const expandedIcon = '👁‍🗨';   // When code is expanded.
-
-        // Define the copy icon
-        const copyIconSymbol = '📋';
+        // Eye - when code is collapsed.
+        const collapsedIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M2.062 12.348a1 1 0 0 1 0-.696a10.75 10.75 0 0 1 19.876 0a1 1 0 0 1 0 .696a10.75 10.75 0 0 1-19.876 0"/><circle cx="12" cy="12" r="3"/></g></svg>';
+        // Crossed eye - when code is expanded.
+        const expandedIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575a1 1 0 0 1 0 .696a10.8 10.8 0 0 1-1.444 2.49m-6.41-.679a3 3 0 0 1-4.242-4.242"/><path d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151a1 1 0 0 1 0-.696a10.75 10.75 0 0 1 4.446-5.143M2 2l20 20"/></g></svg>';
+        // Copy icon
+        const copyIconSymbol = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><rect width="8" height="4" x="8" y="2" rx="1" ry="1"/><path d="M8 4H6a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-2M16 4h2a2 2 0 0 1 2 2v4m1 4H11"/><path d="m15 10l-4 4l4 4"/></g></svg>';
 
         // Select all jsphinx directive containers.
         const containers = document.querySelectorAll(
